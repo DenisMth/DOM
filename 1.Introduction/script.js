b8e0e2995dbf9904f2bc82539cbac903f8f8b64f
Denis Mathieu <denismathieu1203@gmail.com>
2023-12-12 10:02:49 +0100

    launch dom

---
+++ 1.Introduction/script.js
@@ -0,0 +1,18 @@
+document.title = "Hello World !";
+let rgbMax = 255;
+
+function randomNB(max){
+    return Math.floor(Math.random() * max);
+}
+
+let color = `rgb(${randomNB(rgbMax)}, ${randomNB(rgbMax)}, ${randomNB(rgbMax)})`;
+
+let titre = document.title;
+
+document.body.style.backgroundColor = color;
+
+console.log(titre);
+
+for (element of document.body.children){
+    console.log(element);
+}
