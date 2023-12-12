c3d94bbf25af668c8aa5da7fe39f7802b11f9878
Denis Mathieu <denismathieu1203@gmail.com>
2023-12-12 15:01:06 +0100

    added exercises

--- 2.Selectors/script.js
+++ 2.Selectors/script.js
@@ -1,2 +1,33 @@
-/*
-*/
+let rgbMax = 255;
+
+function randomNB(max){
+    return Math.floor(Math.random() * max);
+}
+
+function randomColor(){
+    return `rgb(${randomNB(rgbMax)}, ${randomNB(rgbMax)}, ${randomNB(rgbMax)})`;
+}
+
+const anchor = document.querySelectorAll(".important");
+anchor.title = "This is an important item";
+
+const images = document.querySelectorAll("img");
+for (elem of images){
+    if (elem.className != "important"){
+        elem.style.display = "none";
+    }
+}
+
+const paragraphs = document.querySelectorAll("p");
+for(elem of paragraphs){
+    console.log(elem.innerHTML);
+    elem.style.color = randomColor();
+
+    if (elem.className != ""){
+        console.log(elem.className);
+        elem.style.color = "black";
+
+    }
+}
+
+console.log(anchor);
--- 3.Create/script.js
+++ 3.Create/script.js
@@ -1,2 +1,29 @@
-/*
-*/
+let wilson = ["Noé", "Julien", "Otman", "Aurélien", "Winona", "Alexis", "Damien", "Dylan", "Ugur", "Jérémy", "Mathias", "Maoro", "Robin", "Nicolas", "Stacy", "Alice", "Denis", "Giuseppe", "Alexis", "Cassidy", "Huseyin", "Thomas", "Marvin", "Hanen", "Oksana", "Joshua"];
+
+function randomNB(max){
+    return Math.floor(Math.random() * max);
+}
+
+wilson.sort(() => 0.5 - Math.random());
+
+
+const newSection = document.createElement("section");
+let currentArticle = document.querySelector("article");
+currentArticle.appendChild(newSection);
+
+for (elem of wilson){
+    let newLearner = document.createElement("p");
+    let newText = document.createTextNode(elem);
+    newLearner.appendChild(newText);
+    newSection.appendChild(newLearner);
+
+    let lightness = randomNB(100);
+
+    let chosenColor = `hsl(${randomNB(360)}, ${randomNB(100)}%, ${lightness}%)`;
+
+    newLearner.style.backgroundColor = chosenColor;
+
+    if (lightness < 50){
+        newLearner.style.color = "lightgray";
+    }
+}
